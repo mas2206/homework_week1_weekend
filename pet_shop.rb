ab2f30e962eaabfae97aaa2899b0f2885b28f75c
Michael Shennan <m.shennan22@gmail.com>
2017-03-12 22:29:18 +0000

    Redid homework

--- pet_shop.rb
+++ pet_shop.rb
@@ -24,24 +24,18 @@ end
 
 
 def stock_count(pet_shop)
-  sum = 0
-  for pets in pet_shop[:pets]
-    sum += 1
-  end
-  return sum
-  #return pet_shop[:pets].length()
-  #Also works
+  return pet_shop[:pets].count()
 end
 
 
 def pets_by_breed(pet_shop, breed)
-  count = []
+  breed_array = []
   for pet in pet_shop[:pets]
     if breed == pet[:breed]
-      count << pet
+      breed_array << pet
     end
   end
-  return count
+  return breed_array
 end
 
 
@@ -64,17 +58,19 @@ def remove_pet_by_name(pet_shop, name)
   end
 end
 
+
 def add_pet_to_stock(pet_shop, new_pet)
   pet_shop[:pets] << new_pet
-  return pet_shop[:pets].count
+  return pet_shop[:pets].count()
 end
 
 
 def customer_pet_count(customer)
-  return customer[:pets].count
+  return customer[:pets].count()
 end
 
 
 def add_pet_to_customer(customer, new_pet)
   customer[:pets] << new_pet
+  return customer[:pets].count()
 end
